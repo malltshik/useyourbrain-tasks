e2aabf9d6424908efd85ddc415740c9681fc38c9
Artem Gavrilov <malltshik@gmail.com>
2018-06-10 15:43:43 +0300

    add todos

--- src/main/java/ru/malltshik/tasks/game2048/Game.java
+++ src/main/java/ru/malltshik/tasks/game2048/Game.java
@@ -4,6 +4,10 @@ import ru.malltshik.tasks.game2048.exceptions.GameOverException;
 import ru.malltshik.tasks.game2048.models.Field;
 import ru.malltshik.tasks.game2048.models.Window;
 
+
+// TODO refactorings algorithms (less time & ram complexity)
+// TODO set comments for the classes and methods
+// TODO clean up 
 public class Game {
 
     public static void main(String[] args) throws GameOverException {
